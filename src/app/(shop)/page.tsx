4a045cc86777f895e62116ef8ac9e379f2f9fac8
luisalbeto <luis-alberto11@hotmail.com>
2024-08-15 15:36:31 -0400

    Feat: Creating Project Routes

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <div>
       <h1>Hello world</h1>
       <h1 className={`${titleFont.className} font-bold`}>Hello world</h1>
 
-    </main>
+    </div>
   );
 }
